ee338b67a3f950ccd89b694d19511a119f04eb78
marusoft <alimi.kmaruf@gmail.com>
2020-05-17 14:35:42 +0100

    ft(setup project entries):setup project entries and initialize express

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// make request to the one API for movie data
+async function getMovie(req, res, next) {
+  try {
+    console.log("Fetching Data...");
+
+    let url = "https://the-one-api.herokuapp.com/v1/movie";
+
+    const response = await fetch(url, {
+      headers: { Authorization: process.env.API_KEY },
+    });
+    // console.log(response);
+    const data = await response.json();
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
+}
+
+// Display all movies
+app.get("/movie", getMovie);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT} 🔥`));
